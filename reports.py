1575a202a04792a789e25ebb259f61f58e9c1998
diamond285 <59549446+diamond285@users.noreply.github.com>
2024-05-05 19:10:22 +0500

    Update reports.py

--- reports.py
+++ reports.py
@@ -1,5 +1,13 @@
 import streamlit as st
 
+@st.cache_data
+def load_schools():
+    schools = []
+    with open('schools', 'r') as f:
+        for x in f.readlines():
+            schools.append(x)
+    return schools
+
 password = st.text_input('Введите пароль', type='password')
 
 if password == 'qwerty123!!':
@@ -29,5 +37,36 @@ if password == 'qwerty123!!':
                 label="Скачать отчет",
                 data=buffer,
                 file_name='Дидарлы жаз 2024.xlsx',
+                mime='application/vnd.ms-excel'
+            )
+else:
+    school = st.selectbox("Мектепті таңдаңыз / Выберите школу", ['-'] + load_schools())
+
+    button = st.button("Сформировать отчет")
+    if button and school != '-':
+        with st.spinner('Формирование'):
+            import pandas as pd
+            import psycopg2
+
+            conn = psycopg2.connect(dbname="defaultdb",
+                                    user="avnadmin",
+                                    password="AVNS_T4uu1xCbxFXHvmDVt63",
+                                    host="didarlyzhaz2024-didarlyzhaz2024.e.aivencloud.com",
+                                    port=19026)
+
+            cur = conn.cursor()
+            cur.execute(f'''select * from all_register where "Мектеп / Школа" = '{school}' ''')
+            tmp = cur.fetchall()
+            colnames = [desc[0] for desc in cur.description]
+            df = pd.DataFrame(tmp, columns=colnames).drop(columns='index')
+            import io
+            buffer = io.BytesIO()
+        with pd.ExcelWriter(buffer, engine='xlsxwriter') as writer:
+            df.to_excel(writer, sheet_name='Участники', index=False)
+            writer.close()
+            download2 = st.download_button(
+                label="Скачать отчет",
+                data=buffer,
+                file_name=f'Дидарлы жаз 2024 {school}.xlsx',
                 mime='application/vnd.ms-excel'
             )
